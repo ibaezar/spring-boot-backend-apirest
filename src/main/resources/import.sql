c8d4118fa44b5de9c20f773bea070594627059de
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-08 16:06:18 -0300

    import.sql data added

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+/*Poblando usuarios*/
+
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Izhar', 'Baeza', 'ibaezar@outlook.com', '2022-11-10');
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Liam', 'Baeza', 'liam.baeza@gmail.com', '2022-11-10');
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Valentina', 'García', 'v.garcia@gmail.com', '2022-11-10');
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Victor', 'Baeza', 'victorb@gmail.com', '2022-11-10');
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Levi', 'Baeza', 'levi.b.r@gmail.com', '2022-11-10');
+
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Fabiola', 'reyes', 'freyesp.75@gmail.com', '2022-11-16');
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Manuel', 'Reyes', 'mreyesp.73@gmail.com', '2022-11-16');
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Alex', 'Fuentes', 'afuentesy.8@gmail.com', '2022-11-16');
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Belén', 'Baeza', 'bbaezab@gmail.com', '2022-11-16');
+INSERT INTO client (name, lastname, email, created_at) VALUES ('Kirk', 'hammet', 'khammet@gmail.com', '2022-11-16');
