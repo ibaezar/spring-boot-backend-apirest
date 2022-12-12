dd2e2873714a925c51d6e675adfd9934dc2997d8
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-12 16:58:04 -0300

    methods for CRUD in IclientService and ClientServiceImpl added

--- src/main/java/com/ibaezar/springboot/backend/apirest/app/models/services/ClientServiceImpl.java
+++ src/main/java/com/ibaezar/springboot/backend/apirest/app/models/services/ClientServiceImpl.java
@@ -20,5 +20,22 @@ public class ClientServiceImpl implements IClientService{
     public List<Client> findAll() {
         return (List<Client>) clientDao.findAll();
     }
-    
+
+    @Override
+    @Transactional(readOnly = true)
+    public Client findById(Long id) {
+        return clientDao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public Client save(Client client) {
+        return clientDao.save(client);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        clientDao.deleteById(id);
+    }
 }
