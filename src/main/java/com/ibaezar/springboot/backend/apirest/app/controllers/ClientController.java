40cdb5a70920c794215d88bd7e44dc16676241d3
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-12 16:38:20 -0300

    CORS configured in ClientController

--- src/main/java/com/ibaezar/springboot/backend/apirest/app/controllers/ClientController.java
+++ src/main/java/com/ibaezar/springboot/backend/apirest/app/controllers/ClientController.java
@@ -3,13 +3,16 @@ package com.ibaezar.springboot.backend.apirest.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibaezar.springboot.backend.apirest.app.models.entity.Client;
 import com.ibaezar.springboot.backend.apirest.app.models.services.IClientService;
 
+@CrossOrigin(origins = {"http://localhost:4200/"}, methods = {RequestMethod.GET})
 @RestController
 @RequestMapping("/api/clientes")
 public class ClientController {
